6e99d35a72ded629c9015ad91150532cf520ffca
furkanozen <furkanoozen4@gmail.com>
2026-03-29 07:56:12 +0000

    Remove deprecated CANFD, CANSec, and CANXL header files and associated libraries

--- _install/include/CANSec/CANXLStructure/GlobalThreads.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBALTHREADS_H
-#define GLOBALTHREADS_H
-
-#include <thread>
-inline std::jthread backgroundListeningThread{};
-inline std::jthread backgroundSendingThread{};
-
-#endif // GLOBALTHREADS_H
